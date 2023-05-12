d734ea712c5ffd8b8aae00369ab5b4826bfab512
GC1C_10_サカイレイ <k019g1570@g.neec.ac.jp>
2023-05-12 14:56:52 +0900

    要素の書き込みなど

--- main.cpp
+++ main.cpp
@@ -4,21 +4,14 @@ using namespace std;
 
 int main()
 {
-	//int型配列a(要素数：10)
-	//int a[10];
-	//int型可変長配列b(要素数：0)
-	vector<int>b = { 10,20,30 };
+	vector<float>c = { 0.0f,1.0f,2.0f };
 
-	//40を末尾に追加{10,20,30,40}
-	b.push_back(40);
-	//末尾の要素を削除{10,20,30}
-	b.pop_back();
+	printf("%f\n", c[1]);
 
-	for (int i = 0; i < 3; i++)
-	{
-		printf("%d\n", b[i]);
-	}
+	//c[1]に-1.5fを代入
+	c[1] = -1.5f;
 
-	system("pause");
+	printf("%f\n", c[1]);
+
 	return 0;
 }
