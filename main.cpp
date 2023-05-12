b8539aee612aa728463ecce484310a5a912e166f
GC1C_10_サカイレイ <k019g1570@g.neec.ac.jp>
2023-05-12 15:03:49 +0900

    要素数の変更

--- main.cpp
+++ main.cpp
@@ -4,21 +4,18 @@ using namespace std;
 
 int main()
 {
-	//int型配列a(要素数：10)
-	//int a[10];
-	//int型可変長配列b(要素数：0)
-	vector<int>b = { 10,20,30 };
+	vector<int>d;
 
-	//40を末尾に追加{10,20,30,40}
-	b.push_back(40);
-	//末尾の要素を削除{10,20,30}
-	b.pop_back();
+	//要素数を10個に変更
+	d.resize(10);
 
-	for (int i = 0; i < 3; i++)
+	//要素数を取得
+	int size = d.size();
+
+	for (int i = 0; i < size; i++)
 	{
-		printf("%d\n", b[i]);
+		printf("%d", d[i]);
 	}
 
-	system("pause");
 	return 0;
 }
