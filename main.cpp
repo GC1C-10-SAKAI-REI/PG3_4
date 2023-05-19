ffcf243e0bd157da34dd05cca8554b54c10ec384
GC1C_10_サカイレイ <k019g1570@g.neec.ac.jp>
2023-05-19 12:58:00 +0900

    イテレーターを使い、ベクターの要素を出力

--- main.cpp
+++ main.cpp
@@ -1,10 +1,17 @@
 #include<stdio.h>
+#include <iostream>
 #include <vector>
 using namespace std;
 
 int main()
 {
-	
+	vector<string> f = { "ローソン","ファミマ","セブン" };
+
+	//前要素を順にたどる
+	for (vector<string>::iterator it_f = f.begin();it_f !=f.end();it_f++)
+	{
+		cout << *it_f << endl;
+	}
 
 	return 0;
 }
