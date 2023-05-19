b3c9e8938750b25b3100f266be54fbf28c9f1014
GC1C_10_サカイレイ <k019g1570@g.neec.ac.jp>
2023-05-19 15:14:08 +0900

    要素の削除の仕方

--- main.cpp
+++ main.cpp
@@ -1,11 +1,48 @@
 #include<stdio.h>
 #include <iostream>
-#include <vector>
+#include <list>
 using namespace std;
 
 int main()
 {
-	
+	list<int> lst{ 3,1,4,1,5 };
+	cout << "‰Šúó‘Ô" << endl;
+	for (auto itr = lst.begin(); itr != lst.end(); ++itr)
+	{
+		cout << *itr << ",";
+	}
+	cout << endl;
 
+	//æ“ª‚Ì—v‘f‚ðíœ
+	lst.pop_front();
+	cout << "æ“ª‚Ì—v‘f‚ð”rœ" << endl;
+	for (auto itr = lst.begin(); itr != lst.end(); ++itr)
+	{
+		cout << *itr << ",";
+	}
+	cout << endl;
+
+	//ÅŒã‚Ì—v‘f‚ðíœ
+	lst.pop_back();
+	cout << "ÅŒã‚Ì—v‘f‚ð”rœ" << endl;
+	for (auto itr = lst.begin(); itr != lst.end(); ++itr)
+	{
+		cout << *itr << ",";
+	}
+	cout << endl;
+
+	list<int> lst2{ 3,1,9,4 };
+	auto itr = lst2.begin();
+	++itr;
+	++itr;//2”Ô–Ú‚Ì—v‘f‚ÉˆÚ“®
+	lst2.erase(itr);
+	cout << "2”Ô–Ú‚Ì—v‘f‚ð”rœ" << endl;
+	for (auto itr = lst2.begin(); itr != lst2.end(); ++itr)
+	{
+		cout << *itr << ",";
+	}
+	cout << endl;
+
+	system("pause");
 	return 0;
 }
