4cac5f874f983ef7c643dbf791f6c126d6962dde
GC1C_10_サカイレイ <k019g1570@g.neec.ac.jp>
2023-05-19 13:07:32 +0900

    要素の削除

--- main.cpp
+++ main.cpp
@@ -5,7 +5,17 @@ using namespace std;
 
 int main()
 {
-	
+	vector<int> g = { 1,2,3 };
+
+	//“Y‚¦š1‚Ì—v‘f‚ğíœ{1,3}
+	g.erase(g.begin() + 1);
+	//––”ö‚Ì—v‘f‚ğíœ{1}
+	g.pop_back();
+
+	for (vector<int>::iterator it_g = g.begin(); it_g != g.end(); it_g++)
+	{
+		cout << *it_g << endl;
+	}
 
 	return 0;
 }
