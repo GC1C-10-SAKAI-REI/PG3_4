0dfa4051e5441119ee31184888d83e545b48da8c
GC1C_10_サカイレイ <k019g1570@g.neec.ac.jp>
2023-05-19 12:52:47 +0900

    iteratorの使い方

--- main.cpp
+++ main.cpp
@@ -1,10 +1,26 @@
 #include<stdio.h>
+#include<iostream>
 #include <vector>
+
 using namespace std;
 
 int main()
 {
-	
+	vector<int> e = { 1,2,3 };
+	//int型の可変長配列用のイテレーター
+	vector<int>::iterator it;
+
+	//eの先頭要素を指す
+	it = e.begin();
+
+	//itが指している要素を表示
+	cout << *it << endl;
+
+	//次の要素に移動
+	it++;
+
+	//itが指している要素を表示
+	cout << *it << endl;
 
 	return 0;
 }
